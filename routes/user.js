0d3d1120260b2dd0e64c7c0ee7bc14fe5513b2bd
leegandhi18 <leegandhi18@gmail.com>
2022-01-21 19:50:50 +0900

    express router test

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,43 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
 
-// GET /user 라우터
-router.get('/', (req, res) => {
-  res.send('Hello, User');
+const users = [
+  { id: 0, name: "lee" },
+  { id: 1, name: "kim" },
+  { id: 2, name: "jang" },
+];
+
+router.get("/", (req, res) => {
+  res.status(200).send(users);
+});
+
+
+router.get("/like", (req, res) => {
+  res.send("안녕하세요.");
+});
+
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  const { id } = req.params;
+
+  // if (users[id]) res.status(200).send(users[id]);
+  // else res.status(400).send("값이 없음");
+  users[id]
+    ? res.status(200).send(users[id])
+    : res.status(400).send("값이 없음");
+});
+
+router.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+
+router.post("/", (req, res) => {
+  // console.log(req.body);
+  const newUserData = req.body;
+  users.push(newUserData);
+  res.send();
 });
 
 module.exports = router;
